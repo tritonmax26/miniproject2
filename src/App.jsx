eea296dda6696483aef83d13696f8df0cb819d3d
tritonmax26 <stanleybalmores2013@gmail.com>
2023-04-04 13:23:43 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import NavbarMain from "./components/NavbarMain"
 import Hero from "./Components/Hero"
+import Search from "./Components/Search"
+import Footer from "./Components/Footer"
 
 
 const App = () => {
   return (
     <div>
         <NavbarMain/>        
-        <Hero/>           
+        <Hero/> 
+        <Search/>
+        <Footer/>        
     </div>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+
+      <footer>
+        <h6>&copy; Lancer & Stanley. All rights reserved</h6>
+      </footer>
+    )
+}
+  
+  export default Footer
