e0a56c0424c69ef16578be7ef382b073f5c7972a
tritonmax26 <stanleybalmores2013@gmail.com>
2023-04-06 15:43:05 +0800

    adjusted css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+// import {movies} from "./moviecompo"
+import { Card } from "react-bootstrap";
 
 function Navbar({ onSearch }) {
   const [searchText, setSearchText] = useState('');
@@ -18,22 +20,28 @@ function Navbar({ onSearch }) {
   };
 
   return (
-  
+<div className="">  
     <nav className="navbarSearch">
-      <h3>Search Movie</h3>
-      <div className="search">
-        <input>
-          type="text"
-          placeholder="Search for movies"
-          value={searchText}
-          onChange={handleSearchChange}
-          onKeyUp={handleKeyPress}
-          </input>
-        <button onClick={handleSearch}>
-          Search
-        </button>
-      </div>
-    </nav>    
+      <div className="navwidth">
+          <h3>Search Movie</h3>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Search for movies"
+              value={searchText}
+              onChange={handleSearchChange}
+              onKeyUp={handleKeyPress}
+            >
+            </input>
+            <button onClick={handleSearch}>
+              Search
+            </button>
+          </div>
+      </div>    
+    </nav> 
+
+</div> 
+    
   );
 }
 
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" />
               <Form.Text>
-                We'll email you as soon as we can.
+                
               </Form.Text>
             </Form.Group>
 
